fae82062bc7b2755e667d0cfacf659b62b48cac7
Shawn-Chahal <shawn.chahal@gmail.com>
2020-08-25 20:39:10 -0400

    Fresh Repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Face Generator
+
+This repository trains a Wasserstein Generative Adversarial Network with Gradient Penalty on the [CelebA](http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html) dataset using [TensorFlow](https://github.com/tensorflow/tensorflow) in Python. The generator model is then used in the [Face Generator](https://www.shawnchahal.com/face-generator) web app.
+
+## What does the app do?
+
+[Face Generator](https://www.shawnchahal.com/face-generator) is a web app where the user is shown 64 faces which have been generated using a Generative Adversarial Network. The user can then select as many faces as they'd like to generate a new composite face.
---
+++ flickr-faces-128/latent-flickr-128.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+tf.random.set_seed(1)
+
+z_size = 512
+epoch = 79
+max_sdev = 25
+values = (-max_sdev, max_sdev)
+n_images = z_size * 2
+n_rows = 32
+n_cols = int(n_images / n_rows)
+
+gen_model = tf.keras.models.load_model(os.path.join('objects', 'flickr-faces', f'gen_model_{epoch:03d}.h5'))
+
+fig = plt.figure(figsize=(n_cols, 1.2 * n_rows), dpi=200)
+for i in range(z_size):
+
+    z_vector = np.zeros((2, z_size))
+    z_vector[:, i] = values
+    images = (gen_model(z_vector) + 1) / 2
+
+    for j in range(2):
+        row_index = (i % n_rows)
+        column_index = 2 * int(i / n_rows) + j
+        plot_index = row_index * n_cols + column_index + 1
+        ax = fig.add_subplot(n_rows, n_cols, plot_index)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.text(0.5, 1.1, f'x_{i} = {values[j]}', size=10, horizontalalignment='center', verticalalignment='bottom',
+                transform=ax.transAxes)
+        ax.imshow(images[j])
+
+plt.tight_layout()
+plt.savefig(os.path.join('logs', 'testing', f'latent_{epoch:03d}.png'))
