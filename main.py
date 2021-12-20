7832d8e273169a15e156961dc1b85f577733c62b
Shawn Chahal <shawn.chahal@gmail.com>
2021-12-19 19:21:39 -0500

    Checkpoint

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ def plot_generator_images():
 
 
 tf.random.set_seed(1)
-model_version = 0
+model_version = 101
 
 ReadableTime = namedtuple('ReadableTime', ['days', 'hours', 'minutes', 'seconds'])
 
